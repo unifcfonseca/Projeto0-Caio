84eaf6e02778eb215b95ce52911c7ca7da96947c
caio <unifcfonseca@fei.edu.br>
2024-04-01 15:28:44 -0300

    criar e listar

--- tarefas.c
+++ tarefas.c
@@ -2,7 +2,22 @@
 #include "tarefas.h"
 
 ERROS criar(Tarefa tarefas[], int *pos){
-    printf("Funcao de criar tarefa \n");
+    if(*pos>=TOTAL){
+        return MAX_TAREFA;
+    }
+    printf("Entre com a prioridade: \n");
+    scanf("%d",&tarefas[*pos].prioridade);
+
+    clearBuffer();
+    printf("Entre com a categoria: \n");
+    fgets(tarefas[*pos].categoria, 100, stdin);
+
+
+    printf("Entre com a descricao: \n");
+    fgets(tarefas[*pos].descricao, 300, stdin);
+
+    *pos = *pos + 1;
+
     return OK;
 }
 
@@ -12,7 +27,16 @@ ERROS deletar(Tarefa tarefas[], int *pos) {
 }
 
 ERROS listar(Tarefa tarefas[], int *pos) {
-    printf("Funcao de listar tarefas \n");
+    if(*pos==0){
+        return SEM_TAREFAS;
+    }
+    for (int i = 0; i < *pos; i++) {
+        printf("Pos: %d\t", i+1);
+        printf("Prioridade: %d\t", tarefas[i].prioridade);
+        printf("Categoria: %s\t", tarefas[i].categoria);
+        printf("Descricao: %s\n", tarefas[i].descricao);
+    }
+    return OK;
     return OK;
 }
 
@@ -24,4 +48,9 @@ ERROS salvar(Tarefa tarefas[], int *pos) {
 ERROS carregar(Tarefa tarefas[], int *pos) {
     printf("Funcao de carregar tarefas \n");
     return OK;
+}
+
+void clearBuffer(){
+    int c;
+    while((c = getchar()) != '\n' && c!= EOF) { }
 }
